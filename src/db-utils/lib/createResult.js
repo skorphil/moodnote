e3eaa7a02e65355c5cd45aeee432bfa5281b69eb
Philipp Rich <skorphil@gmail.com>
2024-05-28 15:18:47 +0400

    upd

--- src/db-utils/lib/createResult.js
+++ src/db-utils/lib/createResult.js
@@ -2,11 +2,12 @@ import { dbConnect } from "./dbConnect";
 import Result from "../models/result.model";
 
 export async function createResult(resultData) {
+  console, log("Result:", Result);
   const user = await new Result(resultData);
   try {
     await dbConnect();
     await user.save();
-    console.debug("Result saved to db");
+    console.log("Result saved to db");
   } catch (error) {
     throw error;
   }
